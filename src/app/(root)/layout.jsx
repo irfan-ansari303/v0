6c95e3815a05ffdfb7b1387a039eb86b6f30df2d
irfan ansari <irfa8318450619@gmail.com>
2026-02-14 18:39:04 +0530

    fix bugs in the header

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,5 +1,4 @@
 import { onBoardUser } from "@/modules/auth/actions";
-import Navbar from "@/modules/home/components/Navbar";
 import React from "react";
 
 const Layout = async ({ children }) => { 
@@ -7,7 +6,6 @@ const Layout = async ({ children }) => {
 
   return (
     <main className="flex flex-col min-h-screen relative overflow-x-hidden">
-      <Navbar/>
       {/* Background */}
       <div className="fixed inset-0 -z-10 h-full w-full bg-background dark:bg-[radial-gradient(#393e4a_1px,transparent_1px)] bg-[radial-gradient(#dadde2_1px,transparent_1px)] [background-size:16px_16px]" />
 
